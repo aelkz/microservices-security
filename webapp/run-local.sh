5eb62e02210d73c7c5fce86d34304c240749fa8c
Raphael Alex Silva Abreu <rabreu@redhat.com>
2019-09-18 23:22:04 -0300

    Initial Release

--- webapp/run-local.sh
+++ webapp/run-local.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 export PORT=4200
-export AUTH_URL="https://secure-sso.apps.arekkusu.io/auth"
+export AUTH_URL="https://sso73.apps.arekkusu.io/auth"
 export AUTH_REALM="3scale-api"
-export AUTH_CLIENT_ID="nodejs-web"
-export KEYCLOAK="false"
+export AUTH_CLIENT_ID="8f314bfa"
+export KEYCLOAK="true"
 
-export INTEGRATION_URI="http://localhost:8080"
-export INTEGRATION_HEALTH_URI="http://localhost:8081"
+export INTEGRATION_URI="https://auth-integration-api-production.apps.arekkusu.io"
+export INTEGRATION_HEALTH_URI="https://auth-integration-metrics-api-microservices-security.apps.arekkusu.io"
 export PRODUCT_PATH="/product"
 export STOCK_PATH="/stock"
 export SUPPLIER_PATH="/supplier"
