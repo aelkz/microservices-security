8a83869599dc78b8bf29ddfe125a220f1cc781f1
Raphael Alex Silva Abreu <rabreu@redhat.com>
2019-09-18 21:56:52 -0300

    Initial Release

---
+++ auth-integration/src/main/java/com/microservices/apigateway/security/configuration/FilterRegistrationBean.java
@@ -0,0 +1,26 @@
+package com.microservices.apigateway.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import javax.ws.rs.HttpMethod;
+
+@Configuration
+public class FilterRegistrationBean {
+
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods(HttpMethod.GET.toString(), HttpMethod.POST.toString(),
+                                HttpMethod.PUT.toString(), HttpMethod.DELETE.toString(), HttpMethod.OPTIONS.toString())
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
