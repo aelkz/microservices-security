ebe4cc1d982f9c2ab40b95d82af8ad277a559b28
Raphael Alex Silva Abreu <rabreu@redhat.com>
2019-09-21 23:27:46 -0300

    Initial Release

--- webapp/server.js
+++ webapp/server.js
@@ -16,7 +16,7 @@ let app = express();
 let allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, app_id, app_key, Content-Length');
   next();
 };
 
