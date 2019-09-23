a1a7fc2241ca8205d7b541617fc60b997e07d675
Raphael Alex Silva Abreu <rabreu@redhat.com>
2019-09-23 16:19:57 -0300

    Initial Release

--- webapp/src/app/maintenance/maintenance.service.ts
+++ webapp/src/app/maintenance/maintenance.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import {MessageService} from '../message/message.service';
 import {Observable, of} from 'rxjs';
 import {catchError} from 'rxjs/operators';
@@ -9,27 +9,28 @@ import {Config} from 'codelyzer';
   providedIn: 'root'
 })
 export class MaintenanceService {
+    private maintenanceAPI = window['_env'].integration_uri + '/api/v1';
 
-  constructor(private messageService: MessageService, private http: HttpClient) { }
+    constructor(private messageService: MessageService, private http: HttpClient) { }
 
-  getStockMaintenance(): Observable<HttpResponse<Config>> {
-    return this.http.get<Config>(`${window['_env'].integration_uri}${window['_env'].stock_path}/maintenance`, {observe: 'response'})
-      .pipe(
-        catchError(error => {
-          this.messageService.error(`getStockMaintenance() ${error.message}`);
-            return of(error);
-        })
-      );
-  }
+    getStockMaintenance(): Observable<HttpResponse<Config>> {
+        return this.http.get<Config>(`${this.maintenanceAPI}${window['_env'].stock_path}/maintenance`, {observe: 'response'})
+        .pipe(
+            catchError(error => {
+                this.messageService.error(`getStockMaintenance() ${error.message}`);
+                return of(error);
+            })
+        );
+    }
 
-  getSupplierMaintenance(): Observable<HttpResponse<Config>> {
-    return this.http.get<Config>(`${window['_env'].integration_uri}${window['_env'].supplier_path}/maintenance`, {observe: 'response'})
-      .pipe(
-        catchError(error => {
-          this.messageService.error(`getSupplierMaintenance() ${error.message}`);
-            return of(error);
-        })
-      );
-  }
+    getSupplierMaintenance(): Observable<HttpResponse<Config>> {
+        return this.http.get<Config>(`${this.maintenanceAPI}${window['_env'].supplier_path}/maintenance`, {observe: 'response'})
+        .pipe(
+            catchError(error => {
+                this.messageService.error(`getSupplierMaintenance() ${error.message}`);
+                return of(error);
+            })
+        );
+    }
 
 }
