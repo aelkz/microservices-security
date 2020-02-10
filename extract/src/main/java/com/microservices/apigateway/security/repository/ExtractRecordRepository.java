5eca60056ed18344bbe4aff1d944b5e42b0fd3cf
Raphael Alex Abreu <rabreu@redhat.com>
2020-02-10 10:53:50 -0300

    Initial Release

--- extract/src/main/java/com/microservices/apigateway/security/repository/ExtractRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.apigateway.security.repository;
-
-import com.microservices.apigateway.security.model.ExtractRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExtractRecordRepository extends JpaRepository<ExtractRecord, Long>, ExtractCustomRespository { }
